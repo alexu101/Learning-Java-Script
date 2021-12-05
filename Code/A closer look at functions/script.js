e81d43407382cc9cc7309fe40a3be2094512c0a1
Tanase Alex <alextanase60@yahoo.com>
2021-12-05 11:34:42 +0200

    A closure look to functions done. Closures lectures done

--- Code/A closer look at functions/script.js
+++ Code/A closer look at functions/script.js
@@ -1,5 +1,86 @@
 'use strict';
 
+//Coding Challenge 2
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        if (header.style.color == 'red')
+            header.style.color = 'blue';
+        else header.style.color = 'red';
+    })
+})();
+
+
+
+
+
+/*
+//closures
+const secureBooking = function () {
+    let passengerCount = 0;
+
+    return function () {
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    }
+}
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+//Example 1
+//more closure examples
+let f;
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    }
+}
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    }
+}
+
+
+g();
+f();
+console.dir(f);
+
+//reassigning f function
+h();
+f();
+console.dir(f);
+
+
+
+//Example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+}
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+/*
 const runOnce = function () {
     console.log('This will never run again');
 }
