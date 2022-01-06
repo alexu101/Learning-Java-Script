7bb47161e59a90fcae74f6b1080097e06f90b8bf
Tanase Alex <alextanase60@yahoo.com>
2022-01-06 15:26:50 +0200

    working with styles attributes and classes

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -66,3 +66,33 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(getComputedStyle(message).height);
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+
+//Non standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src'));
+/*
+//Classes
+logo.classList.add();
+logo.classList.remove();
+logo.classList.toggle();
+logo.classList.contains();
+*/
+
+logo.className = 'jonas'
