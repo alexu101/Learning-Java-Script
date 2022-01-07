085e4c73427578fb75364458195542142535b1e3
Tanase Alex <alextanase60@yahoo.com>
2022-01-07 13:04:34 +0200

    event propagation, bubbling and capturing

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -96,7 +96,7 @@ logo.classList.toggle();
 logo.classList.contains();
 
 
-logo.className = 'jonas'*/
+logo.className = 'jonas'
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
@@ -104,4 +104,45 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
-})
+});
+
+//EVENTS
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great you are reading the heading');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+h1.onmouseenter = function (e) {
+  alert('bv');
+}; */
+
+//Event bubbling
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+
+
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target);
+
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target);
+});
+
+
