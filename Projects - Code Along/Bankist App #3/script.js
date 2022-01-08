1f4403f760ee1317d0cbcd99fec3dc2252997e4e
Tanase Alex <alextanase60@yahoo.com>
2022-01-08 11:54:47 +0200

    dom traversing, child, parents and sibling elements

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -182,5 +182,32 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('LINK', e.target);
 });
+
 */
+const h1 = document.querySelector('h1');
+/*
+//Going downwards : child
+console.log(h1.querySelector('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered'
+
+//Going upwards : parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+//h1.closest('.header').style.background = 'var(--gradient-secondary)';
+*/
+
+//Going sideways : siblings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
 
