79ebeee0923ff5f96c62679e44c41874b95a87d3
Tanase Alex <alextanase60@yahoo.com>
2022-01-08 13:40:07 +0200

    passing arguments to event handlers using bind method

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -82,7 +84,27 @@ tabsContainer.addEventListener('click', function (e) {
   clickedContent.classList.add('operations__content--active');
 });
 
+//MENU fade links
+const handleHover = function (e, opacity) {
 
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link)
+        el.style.opacity = this;
+    });
+
+    logo.style.opacity = this;
+  }
+}
+
+//Passing an argument into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 /////////////////////////////////////////////
 ////////////////////////////////////////////
