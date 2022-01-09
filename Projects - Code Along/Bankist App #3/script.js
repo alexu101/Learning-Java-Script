248e5cedf92c452118318e3a3a1752525f33ba87
Tanase Alex <alextanase60@yahoo.com>
2022-01-09 10:27:02 +0200

    sloly revealing section with the intersetion observer

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -130,7 +130,6 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (entry.isIntersecting)
     nav.classList.remove('sticky');
@@ -144,6 +143,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,//te height of the navigation
 });
 headerObserver.observe(header);
+
+//Revealing elements on scroll
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.20,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 
 // not the optimum solution
 /*
