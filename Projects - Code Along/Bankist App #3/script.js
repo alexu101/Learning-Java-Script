17743c6cbf70a74ace0a0a5d0323a4b1befa48e4
Tanase Alex <alextanase60@yahoo.com>
2022-01-10 10:30:53 +0200

    lifecycle dom events : load, beforeunload, domcontentloaded

--- Projects - Code Along/Bankist App #3/script.js
+++ Projects - Code Along/Bankist App #3/script.js
@@ -408,3 +408,18 @@ console.log(h1.nextSibling);
 console.log(h1.parentElement.children);
 */
 
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log(`HTML parsed and DOM tree built!`, e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+
+
+/*window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});*/
